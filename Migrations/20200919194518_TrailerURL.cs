c4ad0f24dba0df6eaaebdf8a79d456b81c649017
Daryl Sonnier <daryl@chaosgamingpc.com>
2020-09-23 10:03:25 -0500

    Initial Checkin
    Initial Checkin.

---
+++ Migrations/20200919194518_TrailerURL.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MvcMovie.Migrations
+{
+    public partial class TrailerURL : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TrailerURL",
+                table: "Movie",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TrailerURL",
+                table: "Movie");
+        }
+    }
+}
